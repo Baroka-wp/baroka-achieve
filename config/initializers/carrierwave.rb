bc302cd7e5ecb3ac0c1d65d9281d4f13ec9f4bec
BAROKA <birotori@gmail.com>
2020-10-22 22:53:07 +0100

    ok

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,9 +16,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'aws-tast'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws-tast'
+      config.asset_host = 'https://s3.console.aws.amazon.com/s3/buckets/aws-tast'
     when 'development'
       config.fog_directory = 'aws-tast'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws-tast'
+      config.asset_host = 'https://s3.console.aws.amazon.com/s3/buckets/aws-tast'
   end
 end
